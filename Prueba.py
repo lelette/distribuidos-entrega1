a7b7e0cbf5d7e050718a1389df7715bd8b35d533
lele <mnicole.arguelles@gmail.com>
2018-05-06 23:28:57 -0400

    avances en contar palabras en un archivo libro.txt (de prueba)

--- Prueba.py
+++ Prueba.py
@@ -2,20 +2,40 @@
 #from mpi4py import MPI
 import sys
 import fileinput
+############################################################################################
 
-size = 3
-rank = 8
-name = "Prueba 1 Distribuidos"
+# Region de funciones
+# ################# funciones para manejo de libros #################
 
-sys.stdout.write(
-    "HOLA MUNDO!!! soy process %d of %d on %s.\n" % (rank,size,name)
-)
 
-file = open("input.txt", "r")
-lines = file.readlines()
-for i in lines:
-   thisline = i.split('"')
-   print (thisline)
+# funcion: contarPalabras 
+# descripcion: dadas las palabras en el chunk correspondiente, las parsea y cuenta para escribir en un archivo de salida
+def contarPalabras(searchFile, bookFile):
+    file = open(searchFile, "r")
+    lines = file.readlines()
+    words = []
+    for i in lines:
+        thisline = i.split('"')
+        words.append(thisline[0])
+        print (words)
+    libro = open(bookFile, "r")
+    lineas = libro.readlines()
+    for word, _ in words:
+        for line  in enumerate(lineas):
+            if lineas[line:line + len(word)] == word:
+                print(word)
+            #if word in line : print("holaa %s" % (word))  
+    return
+
+
+#contarPalabras("input.txt", "libro.txt")
+
+
+#sys.stdout.write(
+#    "HOLA MUNDO!!! soy process %d of %d on %s.\n" % (rank,size,name)
+#)
+
+
 
 tempFile = open( "input.txt", 'r+' )
 
@@ -27,4 +47,5 @@ for line in fileinput.input( "input.txt" ):
     tempFile.write( line.replace( "hardware", "logro" ) )
 
 tempFile.close()
+############################################################################################
 
